647dc0d088c22ed9c1ae8630ca5851d7373f62f2
21851 <austin.pasquariello@vtsd.com>
2022-09-28 11:03:32 -0400

    LaB FINISHED

--- Lab02vst.java
+++ Lab02vst.java
@@ -3,6 +3,22 @@ public class Lab02vst {
     public static void main(String args[]){
         // Example changes
         // My Finished Lab
+        System.out.println("Lab 02, 80 Point Version");
+        System.out.println();
+        System.out.println();
+        System.out.print("Measure today's knowledge ");
+        System.out.println("by yesterday's confusion. ");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+        System.out.print("# ##### #####    ");
+        System.out.print("#####  #####  #  #    # ");
+        System.out.println("##### #####");
+        System.out.print("# #   # #   #    ");
+        System.out.println("#   #  #   #  #  ##   #   #   #    ");
+        System.out.println("# #   # #   #    #####  #   #  #  # #  #   #   #####");
+        System.out.println("# #   # #   #    #      #   #  #  #   ##   #       #");
+        System.out.println("# ##### #####    #      #####  #  #    #   #   #####");
     }
 
 }
